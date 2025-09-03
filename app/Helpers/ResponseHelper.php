6f53421cf551850922abc0302f8396f0168705e3
glendless <glenddelonge@gmail.com>
2025-09-03 15:46:17 +0700

    create models headoffamily and user, migration table

--- app/Helpers/ResponseHelper.php
+++ app/Helpers/ResponseHelper.php
@@ -2,9 +2,11 @@
 
 namespace App\Helpers;
 
+use Illuminate\Http\JsonResponse;
+
 class ResponseHelper
 {
-    public static function jsonResponse($success, $message, $data, $statusCode): R
+    public static function jsonResponse($success, $message, $data, $statusCode): JsonResponse
     {
         return response()->json([
             'success' => $success,
