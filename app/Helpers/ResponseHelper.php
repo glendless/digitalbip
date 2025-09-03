38fc2918574e64b83399df53099af515705092bc
glendless <glenddelonge@gmail.com>
2025-09-03 15:03:29 +0700

    setup project, traits, helper

---
+++ app/Helpers/ResponseHelper.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Helpers;
+
+class ResponseHelper
+{
+    public static function jsonResponse($success, $message, $data, $statusCode): R
+    {
+        return response()->json([
+            'success' => $success,
+            'message' => $message,
+            'data' => $data,
+        ], $statusCode);
+    }
+}
